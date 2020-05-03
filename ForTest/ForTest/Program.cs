67fa946266c05b712973334aa2566c0f42e64df7
CARICOM12 <0190a@ukr.net>
2020-05-03 14:50:05 +0300

    Add files via upload

---
+++ ForTest/ForTest/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace ForTest
+{
+    /*На вход поступает число
+    Надо вывести все целые числа от 0 
+    до этого числа - если это число > 0, 
+    иначе - сообщение "число меньше ноля"
+
+    5
+    0 1 2 3 4 5
+    -5
+    Число меньше ноля */
+
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.Write("Please enter a number: ");
+            int number = Convert.ToInt32(Console.ReadLine());
+            if(number > 0)
+            {
+                for(int i = 0; i <= number; i++)
+                {
+                    Console.Write(i + " ");
+                }
+            }
+
+            else
+            {
+                Console.Write("The number is less than zero");
+            }
+
+
+        }
+    }
+}
